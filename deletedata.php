0fdc86ee2edac5928942bb344429a509950ff0bf
pallo911 <naufalrozaan49@gmail.com>
2026-07-08 21:28:38 +0700

    update banget nih

--- deletedata.php
+++ deletedata.php
@@ -1,37 +1,17 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<?php
+require 'fungsi.php';
 
-    <link rel="stylesheet" href="assets\css\style.css">
+$id = $_GET["id"];
 
 
-    <title>Contact</title>
-  </head>
-  <body>
-    <h1>WEB INFORMATIKA</h1>
-    <hr />
-    <table border="1px" cellspacing="0" cellpadding="10">
-      <tr>
-        <td>
-          <a href="index.php">Home</a>
-        </td>
-        <td>
-          <a href="profile.php">Profile</a>
-        </td>
-        <td>
-          <a href="contact.php">Contact</a>
-        </td>
+if (deletedata($id) > 0)
+    {
+     echo"
+        <script>
+        alert('Data berhasil dihapus!!');
+        window.location.href='Mahasiswa.php';
+        </script>";   
+    }
+    else 
 
-        <td>
-          <a href="Mahasiswa.php">Data Mahasiswa</a>
-        </td>
-        <td>
-          <a href="Inputdata.php">Input Data Mahasiswa</a>
-        </td>
-
-      </tr>
-    </table>
-  </body>
-</html>
+?>
