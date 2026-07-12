e32d1677de618f6215859962a9393aa86779ac5b
pallo911 <naufalrozaan49@gmail.com>
2026-07-13 00:41:58 +0700

    menambah log in session

--- Login.php
+++ Login.php
@@ -1,10 +1,21 @@
 <?php
-    require'fungsi.php';
+session_start();
+require 'fungsi.php';
 
-    if(asset($_POST))
+if (isset($_SESSION["login"])) {
+    header("Location: index.php");
+    exit;
+}
 
+if (isset($_POST["Login"])) {
+    if (login($_POST)) {
+        header("Location: index.php");
+        exit;
+    } else {
+        $error = true;
+    }
+}
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,15 +24,17 @@
     <title>Login</title>
 </head>
 <body>
-    <h1>Login</h1>
-
+    <h2>Login</h2>
+    <?php if (isset($error)) : ?>
+        <p style="color: red; font-style: italic;">username / password salah</p>
+    <?php endif; ?>
     <form action="" method="post">
-        <label for="username">Masukkan Username:</label> <br />
-        <input type="text" name="username" require id="username"><br />
-        <label for="password">Password:</label> <br />
-        <input type="password" name="password" require id="password"><br />
-        <button type="submit" name="login">Login</button>
+        <label for="username">Masukan username:</label>
+        <input type="text" name="username" required id="username"><br><br>
+        <label for="password">Masukan Password:</label>
+        <input type="password" name="password" required id="password"><br><br>
+        <button type="submit" name="Login">login</button>
     </form>
-    <p>Belum punya akun? <a href="register.php">Register!</a> </p>
+    <p>Belum punya akun? <a href="register.php">Daftar</a></p>
 </body>
 </html>
